ac686f0a34164a02683cc69d77d73c2d577f4498
원준석 <applepc24@naver.com>
2025-11-22 21:17:17 +0900

    베포 수정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://43.200.175.25:3000/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
